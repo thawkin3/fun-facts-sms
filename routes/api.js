1c41c943c22511ad3fddf26793ffd119c4af3a7e
Tyler Hawkins <thawkin3@byu.net>
2022-01-16 18:16:46 -0700

    feat: uses node-fetch to make server-side API request

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,22 @@
+const fetch = require('node-fetch');
 const express = require('express');
 const router = express.Router();
 
 router.get('/send-sms/:phoneNumber', function (req, res) {
-  const phoneNumber = req.params.phoneNumber;
-  res.json({ data: phoneNumber });
+  // const phoneNumber = req.params.phoneNumber;
+
+  const URL = 'https://jsonplaceholder.typicode.com/todos';
+
+  fetch(URL)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      res.json(json);
+    })
+    .catch(error => {
+      console.error(error);
+      res.json({ data: error });
+    });
 });
 
 module.exports = router;
